91f109c6928a416ac0103e785252f62d79324702
Thirumani1505 <137150527+Thirumani1505@users.noreply.github.com>
2025-03-21 16:35:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import mean_calc
+import json
+
+#print("enter 9 elements of the matrix: \n")
+
+#lst = [int(i) for i in input().split()]
+
+#print(json.dumps(mean_calc.calculate(lst), indent =4))
+
+print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
